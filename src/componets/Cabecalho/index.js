5e17c6a10e8196e71e358d6aa57fceb823936cc5
silas-mariosa <silas.mariosa@hotmail.com>
2023-05-29 17:03:28 -0300

    #Created compontens

---
+++ src/componets/Cabecalho/index.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+import styles from "./Cabecalho.module.css"
+import logo from "./Logo-cinetag-branco 1.png"
+import CabecalhoLink from "componets/CabecalhoLink"
+
+export default function Cabecalho() {
+  return (
+    <header className={styles.cabecalho}>
+        <Link to={"./"}>
+            <img src={logo} alt="Logo Cine Tag"></img>
+        </Link>
+        <nav>
+            <CabecalhoLink url={"./"}>Home</CabecalhoLink>
+            <CabecalhoLink url={"./Favoritos"}>Favoritos</CabecalhoLink>
+        </nav>
+    </header>
+  )
+}
---
+++ src/pages/Inicio/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Cabecalho from 'componets/Cabecalho'
+import Rodape from 'componets/Rodape'
+
+export default function Inicio() {
+  return (
+    <>
+    <Cabecalho/>
+    <Rodape/>
+    </>
+  )
+}
